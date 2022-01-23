10bade6ffbc5c85c6e53fcc0ba1b4e98923bcaff
taka70gi <taka.70.gi@gmail.com>
2022-01-23 15:59:19 +0900

    [add]ドラマのCRUD処理記載
    CRUD処理の記載と、index画面の簡易作成、imageの関連付けをしました。

--- app/controllers/dramas_controller.rb
+++ app/controllers/dramas_controller.rb
@@ -1,22 +1,36 @@
 class DramasController < ApplicationController
   def index
-  end
-
-  def new
-  end
-
-  def create
+    @dramas = Drama.all
   end
 
   def show
+    @drama = Drama.find(params[:id])
   end
 
-  def edit
+  def new
+    @drama = Drama.all
   end
 
-  def update
+  def create
+    @drama = Drama.new(dramas_params)
+    if @drama.save
+      flash[:notice] = "ドラマを新規登録しました"
+      redirect_to :dramas
+    else
+      flash.now[:alert] = "ルームを登録できませんでした"
+      render new_drama_path
+    end
   end
 
   def destroy
+    @drama = Drama.find(params[:id])
+    @drama.destroy
+    flash[:notice] = "ユーザーを削除しました"
+    redirect_to :dramas
   end
+end
+
+private
+def dramas_params
+  params.require(:drama).permit(:name, :release_day, :image, :summary, :cast)
 end
--- app/models/drama.rb
+++ app/models/drama.rb
@@ -1,2 +1,3 @@
 class Drama < ApplicationRecord
+  has_one_attached :image
 end
