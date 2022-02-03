58dd2745229d25191083fef643d6c4af80e4e3ea
taka70gi <taka.70.gi@gmail.com>
2022-02-03 22:28:05 +0900

    [update]ユーザー画像プレビュー機能追加
    ユーザー画像を編集する際に、プレビューが画面表示されるようにjQueryを作成しました。

--- app/javascript/style.js
+++ app/javascript/style.js
@@ -17,4 +17,19 @@ document.addEventListener("turbolinks:load", function() {
         $(".header-nav-menu").slideToggle(200);
     })
   })
+  $(function() {
+    function readURL(input) {
+      if (input.files && input.files[0]) {
+        var reader = new FileReader();
+        reader.onload = function (e) {
+          $('.img_prev').attr('src', e.target.result);
+        }
+        reader.readAsDataURL(input.files[0]);
+      }
+    }
+    $(".post_post_img").change(function() {
+      $('.img_prev').removeClass('hidden');
+      readURL(this);
+    });
+  });
 });
