b3d1066889f74c9e6bdfc8543a8ebe8b1fd7bb3e
taka70gi <taka.70.gi@gmail.com>
2022-02-04 21:34:24 +0900

    [update]コメント更新機能追加
    commentの更新機能を追加し、新規投稿画面とレイアウトを揃えました。

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,45 +1,56 @@
 class CommentsController < ApplicationController
+  before_action :authenticate_user!
   def index
     @user = current_user
     @comments = @user.comments
   end
 
   def new
+    @user = current_user
+    @comment = Comment.new
   end
 
   def create
-    @comment = Comment.new(params.require(:comment).permit(:content, :user_id, :drama_id))
-    @drama = Drama.find(params[:comment][:drama_id])
+    @comment = Comment.new(comments_params)
     if @comment.save
-      flash[:notice] = "新規登録"
-      redirect_to drama_path(@drama)
+      flash[:notice] = "投稿しました"
+      redirect_to drama_path(@comment.drama)
     else
-      render "comments"
+      flash.now[:alert] = "投稿できませんでした"
+      render drama_path(@comment)
     end
   end
 
   def show
     @user = current_user
     @comment = Comment.new
-    @drama = Drama.find(params[:id])
   end
 
   def edit
+    @user = current_user
+    @comment = Comment.find(params[:id])
   end
 
   def update
+    @comment = Comment.find(params[:id])
+    if @comment.update(comments_params)
+      flash[:notice] = "コメントを更新しました"
+      redirect_to users_path
+    else
+      flash[:alert] = "コメントを更新できませんでした"
+      render users_path
+    end
   end
 
   def destroy
     @comment = Comment.find(params[:id])
     @comment.destroy
-    flash[:notice] = "ユーザーを削除しました"
+    flash[:notice] = "コメントを削除しました"
     redirect_to :users
   end
-  private
-  def comments_params
-    params.require(:comment).permit(:content, :user_id, :drama_id)
-  end
+end
 
-
+private
+def comments_params
+  params.require(:comment).permit(:content, :user_id, :drama_id)
 end
