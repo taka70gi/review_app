160ad1552fd463ff95e49438d42c9caa2db48847
taka70gi <taka.70.gi@gmail.com>
2022-02-05 17:32:30 +0900

    [add]ページタイトルの作成
    各ページでタイトルが表示されるようにhelperメソッドを作成しました。

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,4 +1,10 @@
 module ApplicationHelper
+  BASE_TITLE = "韓ドラレビュー".freeze
+
+  def full_title(pagetitle)
+    pagetitle.blank? ? BASE_TITLE : "#{pagetitle} - #{BASE_TITLE}"
+  end
+
   def load_user
     return current_user
   end
