571da728e868e6eed8614956467b0ab610084721
taka70gi <taka.70.gi@gmail.com>
2022-02-09 21:59:39 +0900

    [update]ログイン後の画面遷移修正
    管理者ログイン機能を追加したため、ログイン後の画面を管理者、一般で分けました。

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,4 +4,12 @@ class ApplicationController < ActionController::Base
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
   end
+
+  def after_sign_in_path_for(resource)
+    if current_user.admin?
+      adminhomes_path
+    else
+      edit_user_path(current_user)
+    end
+  end
 end
