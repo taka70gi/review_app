552b923e709be88674bd5b005d55f83839853851
taka70gi <taka.70.gi@gmail.com>
2022-02-10 20:45:04 +0900

    [remove]テストフォルダ削除
    不要なテストフォルダを削除しました。

--- test/controllers/dramas_controller_test.rb
+++
@@ -1,8 +0,0 @@
-require "test_helper"
-
-class DramasControllerTest < ActionDispatch::IntegrationTest
-  test "should get index" do
-    get dramas_index_url
-    assert_response :success
-  end
-end
