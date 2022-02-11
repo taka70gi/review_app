c5de4fb2283ca9e8439ad31d7430c3e428351e0f
taka70gi <taka.70.gi@gmail.com>
2022-02-11 19:58:26 +0900

    [fix]コントローラー名修正に伴う修正漏れ
    コントローラー名をadminhomesからhomesへ変更した際のパス修正漏れを対応しました。

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,7 +7,7 @@ class ApplicationController < ActionController::Base
 
   def after_sign_in_path_for(resource)
     if current_user.admin?
-      adminhomes_path
+      homes_path
     else
       dramas_path
     end
