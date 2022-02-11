2660052c2886943a937505cf619f7ab4415b04f2
taka70gi <taka.70.gi@gmail.com>
2022-02-12 01:49:30 +0900

    [fix]commentのrequestテスト漏れの追加
    viewが存在するもののみのテストしかしていなかったので、
    テストケースを追加しました。

--- spec/requests/comment_spec.rb
+++ spec/requests/comment_spec.rb
@@ -66,4 +66,126 @@ RSpec.describe "Comments", type: :request do
       expect(response.body).to include comment.content
     end
   end
+
+  describe "create" do
+    let!(:general) { create :user, :general }
+    let!(:comment) { create(:comment) }
+
+    before do
+      sign_in general
+    end
+    context 'パラメータが妥当な場合' do
+      it 'レスポンスのステータスコードが302であるか' do
+        post comments_path, params: { comment: {content: comment.content, user_id: comment.user_id, drama_id: comment.drama_id }}
+        expect(response.status).to eq 302
+      end
+
+      it 'レビューが登録されるか' do
+        expect do
+          post comments_path, params: { comment: {content: comment.content, user_id: comment.user_id, drama_id: comment.drama_id }}
+        end.to change(Comment, :count).by(1)
+      end
+
+      it 'レビュー登録後ドラマ詳細画面へリダイレクトするか' do
+        post comments_path, params: { comment: {content: comment.content, user_id: comment.user_id, drama_id: comment.drama_id }}
+        expect(response).to redirect_to drama_path(comment.drama)
+      end
+    end
+
+    context 'パラメータが不正な場合' do
+      it 'レスポンスのステータスコードが200であるか' do
+        post comments_path, params: { comment: {user_id: comment.user_id, drama_id: comment.drama_id }}
+        expect(response.status).to eq 200
+      end
+
+      it 'レビューが登録されないか' do
+        expect do
+          post comments_path, params: { comment: {user_id: comment.user_id, drama_id: comment.drama_id }}
+        end.to change(Comment, :count).by(0)
+      end
+
+      it 'editテンプレートで表示されるか' do
+        post comments_path, params: { comment: {user_id: comment.user_id, drama_id: comment.drama_id }}
+        expect(response).to render_template :edit
+      end
+
+      it '”レビューを投稿できませんでした”のエラーが表示されること' do
+        patch comment_path(comment), params: { comment: {content: "", user_id: comment.user_id }}
+        expect(response.body).to include "レビューを更新できませんでした"
+      end
+    end
+  end
+
+  describe "update" do
+    let!(:general) { create :user, :general }
+    let!(:comment) { create(:comment) }
+    before do
+      sign_in general
+    end
+
+    context 'パラメータが妥当な場合' do
+      it 'レスポンスのステータスコードが302であるか' do
+        patch comment_path(comment), params: { comment: {content: "アップデート", user_id: comment.user_id, drama_id: comment.drama_id }}
+        expect(response.status).to eq 302
+      end
+
+      it 'レビュー内容が更新されること' do
+        expect do
+          patch comment_path(comment), params: { comment: {content: "アップデート", user_id: comment.user_id, drama_id: comment.drama_id }}
+        end.to change { Comment.find(comment.id).content }.from("テストデータ").to("アップデート")
+      end
+
+      it 'マイページへリダイレクトすること' do
+        patch comment_path(comment), params: { comment: {content: "アップデート", user_id: comment.user_id, drama_id: comment.drama_id }}
+        expect(response).to redirect_to users_path
+      end
+    end
+
+    context 'パラメータが不正な場合' do
+      it 'レスポンスのステータスコードが200であるか' do
+        patch comment_path(comment), params: { comment: {content: "", user_id: comment.user_id }}
+        expect(response.status).to eq 200
+      end
+
+      it 'レビュー内容が更新されないこと' do
+        expect do
+          patch comment_path(comment), params: { comment: {content: "", user_id: comment.user_id }}
+        end.to_not change(Comment.find(comment.id), :content)
+      end
+
+      it 'editテンプレートで表示されるか' do
+        patch comment_path(comment), params: { comment: {content: "", user_id: comment.user_id }}
+        expect(response).to render_template :edit
+      end
+
+      it '”レビューを更新できませんでした”のエラーが表示されること' do
+        patch comment_path(comment), params: { comment: {content: "", user_id: comment.user_id }}
+        expect(response.body).to include "レビューを更新できませんでした"
+      end
+    end
+  end
+
+  describe "delete" do
+    let!(:general) { create :user, :general }
+    let!(:comment) { create(:comment) }
+    before do
+      sign_in general
+    end
+
+    it 'レスポンスのステータスコードが302であるか' do
+      delete comment_path(comment)
+      expect(response.status).to eq 302
+    end
+
+    it 'レビューが削除されること' do
+      expect do
+        delete comment_path(comment)
+      end.to change(Comment, :count).by(-1)
+    end
+
+    it 'マイページにリダイレクトすること' do
+      delete comment_path(comment)
+      expect(response).to redirect_to(users_path)
+    end
+  end
 end
