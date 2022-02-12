2a43f3e491e5b9bbe4a8dc6992e3bdc4fa051826
taka70gi <taka.70.gi@gmail.com>
2022-02-12 17:17:36 +0900

    [fix]テストケースの文言修正
    テストケースのタイトル文言を揃えました。

---
+++ spec/requests/homes_spec.rb
@@ -0,0 +1,16 @@
+require 'rails_helper'
+
+RSpec.describe "Homes", type: :request do
+  describe "index" do
+    let!(:admin) { create :user, :admin }
+
+    before do
+      sign_in admin
+      get homes_path
+    end
+
+    it 'レスポンスのステータスコードが200であること' do
+      expect(response.status).to eq 200
+    end
+  end
+end
