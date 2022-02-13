e4da570eda00abbb8d394c807e860460013aa378
taka70gi <taka.70.gi@gmail.com>
2022-02-13 17:21:29 +0900

    [update]管理者ページの結合テスト作成
    管理者ページ(homes)の結合テストの作成をしました。

--- spec/features/homes_spec.rb
+++ spec/features/homes_spec.rb
@@ -2,11 +2,14 @@ require 'rails_helper'
 
 feature 'homes', type: :feature do
   describe "index" do
-    given!(:admin) { create :user, :admin }
-    given!(:drama) { create(:drama) }
-    given(:image) { create(:image) }
+    given(:admin) { create :user, :admin }
+    given(:general) { create :user, :general }
+    given(:drama) { create(:drama) }
+    given(:comment) { create(:comment) }
     before do
       sign_in admin
+      general.comments << comment
+      drama.comments << comment
       visit homes_path
     end
 
@@ -16,6 +19,8 @@ feature 'homes', type: :feature do
         expect(current_path).to eq admin_dramas_path
       end
       expect(page).to have_content("ドラマ管理ページ")
+      expect(page).to have_content(drama.name)
+      expect(page).to have_selector("img,[src$='#{drama.image.filename}']")
     end
 
     it 'レビュー管理ページへのリンクが正しくされていること' do
@@ -23,7 +28,15 @@ feature 'homes', type: :feature do
         click_on "レビュー管理"
         expect(current_path).to eq comments_path
       end
-      expect(page).to have_content("ドラマ管理ページ")
+      expect(page).to have_content("レビュー管理ページ")
+      expect(page).to have_content(drama.name)
+      expect(page).to have_selector("img,[src$='#{drama.image.filename}']")
+      drama.comments.each{|comment|
+        expect(page).to have_content(comment.user.name)
+      }
+      expect(page).to have_content(general.name)
+      expect(page).to have_content(general.name)
     end
   end
+
 end
