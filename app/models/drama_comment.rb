c2bcd464f316a078bff142e8873d1e59d52af22b
taka70gi <taka.70.gi@gmail.com>
2022-02-14 20:34:39 +0900

    [delete]不要ファイル削除
    中間モデル削除、テスト用の画像の削除をしました。

--- app/models/drama_comment.rb
+++
@@ -1,4 +0,0 @@
-class DramaComment < ApplicationRecord
-  belongs_to :drama
-  belongs_to :comment
-end
--- spec/factories/images.rb
+++
@@ -1,5 +0,0 @@
-FactoryBot.define do
-  factory :image do
-    image { "spec/fixtures/star_img.png" }
-  end
-end
