849032301963fb59e58b7acc2f31898f80c38d96
taka70gi <taka.70.gi@gmail.com>
2022-02-14 18:44:24 +0900

    [update]ヘッダー部の結合テスト作成
    ヘッダー部の結合テストを作成しました。

--- spec/features/applications_spec.rb
+++ spec/features/applications_spec.rb
@@ -1,111 +1,225 @@
 require 'rails_helper'
 
 feature 'application', type: :feature do
-  describe "header" do
-    given!(:drama) { create(:drama) }
+  describe "ヘッダー" do
+    describe "検索" do
+      describe "検索ワード有" do
+        given!(:drama01) { create(:drama, name: "太陽の末裔") }
+        given!(:drama02) { create(:drama, name: "トッケビ") }
+        given!(:drama03) { create(:drama, name: "ボーイフレンド") }
+        describe '検索結果有' do
+          before do
+            visit dramas_path
+            fill_in 'keyword', with: "太陽の末裔"
+            click_on "検索"
+          end
+          it '検索件数が登録ドラマ件数で表示されること' do
+            within("div.search_drama") do
+              expect(page).to have_content("検索結果:1件")
+            end
+          end
 
-    before do
-      visit dramas_path
-    end
+          it '検索ワードが表示されること' do
+            within("div.search_drama") do
+              expect(page).to have_content("太陽の末裔")
+            end
+          end
 
-    it 'ページタイトルが表示されること' do
-      within("div.header_title") do
-        expect(page).to have_content("韓ドラレビュー")
+          it '検索結果が存在する場合ドラマ情報が表示されること' do
+            within("div.drama_list") do
+              expect(page).to have_content(drama01.name)
+            end
+          end
+
+          it '検索結果以外のドラマ情報が表示されないこと' do
+            within("div.drama_list") do
+              expect(page).to_not have_content(drama02.name)
+              expect(page).to_not have_content(drama03.name)
+            end
+          end
+
+          it 'ドラマ一覧ページへのリンクが正しくされていること' do
+            within("div.drama_list") do
+            end
+          end
+        end
+        describe '検索結果無' do
+          before do
+            visit dramas_path
+            fill_in 'keyword', with: "梨泰院クラス"
+            click_on "検索"
+          end
+          it '検索件数が登録0件で表示されること' do
+            within("div.search_drama") do
+              expect(page).to have_content("検索結果:0件")
+            end
+          end
+
+          it '検索ワードが表示されること' do
+            within("div.search_drama") do
+              expect(page).to have_content("梨泰院クラス")
+            end
+          end
+
+          it 'ドラマ情報が表示されないこと' do
+            expect(page).to_not have_content(drama01.name)
+            expect(page).to_not have_content(drama02.name)
+            expect(page).to_not have_content(drama03.name)
+          end
+
+          it 'ドラマ一覧ページへのリンクが正しくされていること' do
+            within("div.return_link_position") do
+              click_on "一覧に戻る"
+              expect(current_path).to eq dramas_path
+            end
+          end
+        end
+      end
+
+      describe "検索ワード無" do
+        given!(:drama01) { create(:drama, name: "太陽の末裔") }
+        given!(:drama02) { create(:drama, name: "トッケビ") }
+        given!(:drama03) { create(:drama, name: "ボーイフレンド") }
+        before do
+          visit dramas_path
+          fill_in 'keyword', with: ""
+          click_on "検索"
+        end
+
+        it '検索件数が登録ドラマ件数で表示されること' do
+          within("div.search_drama") do
+            expect(page).to have_content("検索結果:3件")
+          end
+        end
+
+        it '全ドラマが表示されること' do
+          expect(page.all('.drama_img').count).to eq 3
+        end
+
+        it 'ドラマタイトルが表示されること' do
+          within("div.list") do
+            expect(page).to have_content(drama01.name)
+            expect(page).to have_content(drama02.name)
+            expect(page).to have_content(drama03.name)
+          end
+        end
+
+        it 'ドラマ画像が表示されること' do
+          within("div.list") do
+            expect(page).to have_selector("img,[src$='#{drama01.image.filename}']")
+            expect(page).to have_selector("img,[src$='#{drama02.image.filename}']")
+            expect(page).to have_selector("img,[src$='#{drama02.image.filename}']")
+          end
+        end
+
+        it 'ドラマ一覧ページへのリンクが正しくされていること' do
+          within("div.return_link_position") do
+            click_on "一覧に戻る"
+            expect(current_path).to eq dramas_path
+          end
+        end
       end
     end
 
-    it '検索のリンクが正しくされていること' do
-      within("div.header_search_field") do
-        click_on "#{taxon.name}(#{taxon.products.count})"
-        expect(current_path).to eq potepan_category_path(taxon.id)
+    describe "メニューバー" do
+      describe "管理者ログイン" do
+        given!(:admin) { create :user, :admin }
+        given!(:drama) { create(:drama) }
+        before do
+          sign_in admin
+          visit dramas_path
+        end
+
+        it '管理者ユーザーが表示されること' do
+          within("div.header_name") do
+            expect(page).to have_content(admin.name)
+          end
+        end
+
+        it '管理者ユーザー用のナビバーが表示されること' do
+          within("ul.header-nav-menu") do
+            expect(page).to have_content("管理者ページ")
+            expect(page).to have_content("ドラマ管理ページ")
+            expect(page).to have_content("レビュー管理ページ")
+            expect(page).to have_content("ログアウト")
+          end
+        end
+
+        it '管理者ページのリンクが正しくされていること' do
+          within("ul.header-nav-menu") do
+            click_on "管理者ページ"
+            expect(current_path).to eq homes_path
+          end
+        end
+
+        it 'ドラマ管理ページのリンクが正しくされていること' do
+          within("ul.header-nav-menu") do
+            click_on "ドラマ管理ページ"
+            expect(current_path).to eq admin_dramas_path
+          end
+        end
+
+        it 'レビュー管理ページのリンクが正しくされていること' do
+          within("ul.header-nav-menu") do
+            click_on "レビュー管理ページ"
+            expect(current_path).to eq comments_path
+          end
+        end
+
+        it 'ログアウトのリンクが正しくされていること' do
+          within("ul.header-nav-menu") do
+            click_on "ログアウト"
+            expect(current_path).to eq dramas_path
+          end
+        end
       end
-      expect(page).to have_content(taxon.name)
-    end
-  end
 
-  describe "admin" do
-    given!(:admin) { create :user, :admin }
-    given!(:drama) { create(:drama) }
-    before do
-      sign_in admin
-      visit dramas_path
-    end
+      describe "一般ログイン" do
+        given!(:general) { create :user, :general }
+        given!(:drama) { create(:drama) }
+        before do
+          sign_in general
+          visit dramas_path
+        end
 
-    it '管理者ユーザー表示されること' do
-      within("div.header_name") do
-        expect(page).to have_content(admin.name)
+        it 'ログインユーザー表示されること' do
+          within("div.header_name") do
+            expect(page).to have_content(general.name)
+          end
+        end
+
+        it '一般ユーザー用のナビバーが表示されること' do
+          within("ul.header-nav-menu") do
+            expect(page).to have_content("マイページ")
+            expect(page).to have_content("アカウント編集")
+            expect(page).to have_content("ログアウト")
+          end
+        end
+
+        it 'マイページのリンクが正しくされていること' do
+          within("ul.header-nav-menu") do
+            click_on "マイページ"
+            expect(current_path).to eq users_path
+          end
+        end
+
+        it 'アカウント編集ページのリンクが正しくされていること' do
+          within("ul.header-nav-menu") do
+            click_on "アカウント編集"
+            expect(current_path).to eq edit_user_path(general)
+          end
+        end
+
+        it 'ログアウトのリンクが正しくされていること' do
+          within("ul.header-nav-menu") do
+            click_on "ログアウト"
+            logout general
+            binding.pry
+            expect(page).to have_current_path(dramas_path)
+            expect(current_path).to eq dramas_path
+          end
+        end
       end
-    end
-
-    it '管理者ユーザー用のナビバーが表示されること' do
-      within("ul.header-nav-menu") do
-        expect(page).to have_content("ドラマ管理ページ")
-        expect(page).to have_content("ドラマ新規登録")
-        expect(page).to have_content("ログアウト")
-      end
-    end
-
-    it 'ドラマ管理ページのリンクが正しくされていること' do
-      within("ul.header-nav-menu") do
-        click_on "ドラマ管理ページ"
-        expect(current_path).to eq admin_dramas_path
-      end
-      expect(page).to have_content("ドラマ管理ページ")
-    end
-
-    it 'ドラマ新規登録のリンクが正しくされていること' do
-      within("ul.header-nav-menu") do
-        click_on "ドラマ新規登録"
-        expect(current_path).to eq new_admin_drama_path
-      end
-      expect(page).to have_content("ドラマ新規登録")
-    end
-
-    it 'ログアウトのリンクが正しくされていること' do
-      within("ul.header-nav-menu") do
-        click_on "ログアウト"
-        expect(current_path).to eq destroy_user_session_path
-      end
-      expect(page).to have_content("ドラマ新規登録")
-    end
-  end
-
-  describe "general" do
-    given!(:general) { create :user, :general }
-    given!(:drama) { create(:drama) }
-    before do
-      sign_in general
-      visit dramas_path
-    end
-
-    it '一般ユーザー表示されること' do
-      within("div.header_name") do
-        expect(page).to have_content(general.name)
-      end
-    end
-
-    it '一般ユーザー用のナビバーが表示されること' do
-      within("ul.header-nav-menu") do
-        expect(page).to have_content("マイページ")
-        expect(page).to have_content("アカウント編集")
-        expect(page).to have_content("ログアウト")
-      end
-    end
-
-    it 'マイページのリンクが正しくされていること' do
-      within("ul.header-nav-menu") do
-        click_on "マイページ"
-        expect(current_path).to eq users_path
-      end
-      expect(page).to have_content("マイページ")
-    end
-
-    it 'アカウント編集のリンクが正しくされていること' do
-      within("ul.header-nav-menu") do
-        click_on "アカウント編集"
-        expect(current_path).to eq edit_user_path(general)
-      end
-      expect(page).to have_content("アカウント編集")
     end
   end
 end
