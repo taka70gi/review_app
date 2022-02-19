de4fafac45cd46cb4c5a8b8fc9c4b9c36b877aca
taka70gi <taka.70.gi@gmail.com>
2022-02-19 16:29:08 +0900

    [update]お気に入り機能非同期
    お気に入り機能を非同期実装に修正しました。

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -1,26 +1,17 @@
 class FavoritesController < ApplicationController
   before_action :authenticate_user!
+  before_action :target_find
 
   def create
-    @drama = Drama.find(params[:drama_id])
-    favorite = @drama.favorites.new(user_id: current_user.id)
-    if favorite.save
-      flash[:notice] = "お気に入り登録しました"
-      redirect_to drama_path(@drama)
-    else
-      redirect_to drama_path(@drama)
-    end
+    Favorite.create(user_id: current_user.id, drama_id: params[:drama_id])
   end
 
   def destroy
+    favorite = Favorite.find_by(user_id: current_user.id, drama_id: params[:drama_id])
+    favorite.destroy
+  end
+
+  def target_find
     @drama = Drama.find(params[:drama_id])
-    favorite = @drama.favorites.find_by(user_id: current_user.id)
-    if favorite.present?
-      flash[:notice] = "お気に入り解除しました"
-      favorite.destroy
-      redirect_to drama_path(@drama)
-    else
-      redirect_to drama_path(@drama)
-    end
   end
 end
